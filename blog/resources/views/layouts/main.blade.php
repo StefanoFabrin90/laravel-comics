837db5b713982ffb71ac2d0c34020194800e738f
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-19 15:43:48 +0100

    add 3

--- blog/resources/views/layouts/main.blade.php
+++ blog/resources/views/layouts/main.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     {{-- header --}}
     @include('partials.header')
 
     {{-- main --}}
-    <main>
+    <main  class="container">
         @yield('main-content')
     </main>
 
