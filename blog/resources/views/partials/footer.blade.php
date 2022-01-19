6f73523b2d2b493eefe92220b2b6d5761d7b793d
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-19 15:04:31 +0100

    add 1

---
+++ blog/resources/views/partials/footer.blade.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="container">
+        <div class="column-right">
+            <button>sing-up now!</button>
+        </div>
+        <div class="column-left">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('images/footer-facebook.png') }}" alt="facebook">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>
---
+++ blog/resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="/">Logo</a>
+            </li>
+            <li>
+                <a href="">Comics</a>
+            </li>
+            <li>
+                <a href="">News</a>
+            </li>
+        </ul>
+    </nav>
+</header>
