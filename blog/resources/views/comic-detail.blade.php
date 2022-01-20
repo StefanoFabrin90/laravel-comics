0d20a8be2a7c5e4f156823e128b4bc6171f44921
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-20 17:29:40 +0100

    add 3

--- blog/resources/views/comic-detail.blade.php
+++ blog/resources/views/comic-detail.blade.php
@@ -1,34 +1,38 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <section class="image-detail">
+    <section class="image-poster">
         {{-- <img src="../images/jumbotron.jpg" alt=""> --}}
+        <img src={{ $comic['thumb'] }} alt=""> 
+    </section>
+    <section class="lightblue">
     </section>
     <section class="container">
-        <div class="detail">
-            <h1>{{ $comic['title'] }}</h1>
-            <div>
-                <div class="check">
-                    <div class="price">
-                        U.S Price: {{ $comic['price'] }}
-                        <span class="available">Available</span>
-                    </div>
-                    <div class="dropdown">
-                        <button class="dropbtn"> Dropdown </button>
-                        <div class="dropdown-content">
-                            <a href="#"> Link 1 </a>
-                            <a href="#"> Link 2 </a>
+        <div class="aboutcomic">
+            <div class="detail">
+                <h1>{{ $comic['title'] }}</h1>
+                <div>
+                    <div class="check">
+                        <div class="price">
+                            U.S Price: {{ $comic['price'] }}
+                            <span class="available">Available</span>
+                        </div>
+                        <div class="dropdown">
+                            <button class="dropbtn"> Check Availability </button>
+                            <div class="dropdown-content">
+                                <a href="#"> Link 1 </a>
+                                <a href="#"> Link 2 </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="text">
-                {{ $comic['description'] }}
+                <div class="text">
+                    {{ $comic['description'] }}
+                </div>
             </div>
             <figure>
                 <img src="../images/adv.jpg" alt="">
             </figure>
         </div>
-        
     </section>
 @endsection 
